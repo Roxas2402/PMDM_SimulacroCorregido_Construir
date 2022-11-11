de0c7db631099ec731737ff42a55fe5433829a0a
roxas <jeropastrana@gmail.com>
2022-11-11 16:31:55 +0100

    a

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder frase = new StringBuilder();
+        String palabra;
+
+        while (!(palabra = sc.nextLine()).equalsIgnoreCase("FIN")) {
+            frase.append(palabra).append(" ");
+        }
+
+        System.out.println(frase.toString().trim());
     }
 }
